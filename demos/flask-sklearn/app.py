11cddffa78003ffac95b70705eb1056c9526e8f6
Vikram Panth <vikrampanth@gmail.com>
2021-06-28 19:39:02 +0530

    first commit

--- demos/flask-sklearn/app.py
+++ demos/flask-sklearn/app.py
@@ -3,7 +3,8 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-from sklearn.externals import joblib
+import sklearn.external.joblib as extjoblib
+import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
